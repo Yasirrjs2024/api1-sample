9a8f28ec1a920becb5ad3cbb24f247eef2c374c2
Yasirrjs2024 <roport19632024@gmail.com>
2025-11-12 00:56:24 +0300

    Initial commit: simple API for CBH test

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Dockerfile
+# ---- Build stage
+FROM maven:3.9.8-eclipse-temurin-21 AS build
+WORKDIR /app
+COPY pom.xml .
+COPY src ./src
+RUN mvn -q -e -DskipTests=true package
+
+# ---- Runtime stage
+FROM eclipse-temurin:21-jre
+ENV SPRING_PROFILES_ACTIVE=docker
+ENV SPRING_SERVER_PORT=8080
+WORKDIR /app
+COPY --from=build /app/target/*.jar app.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","/app/app.jar"]
---
+++ src/main/java/com/yasir/api1sample/Api1SampleApplication.java
@@ -0,0 +1,20 @@
+package com.yasir.api1sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+@SpringBootApplication
+@RestController
+@CrossOrigin
+public class Api1SampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Api1SampleApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello() {
+        var rnd = ThreadLocalRandom.current().nextInt(0, 1000);
+        return "Hello from api1-sample! Random = " + rnd;
+    }
+}
